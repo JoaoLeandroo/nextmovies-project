a6bcb7cff20e397b501eae02c1a11e0c23634925
joaoleandro <new.joaoleandro@gmail.com>
2024-06-05 20:10:39 -0300

    chore: change environment variable name

--- src/app/api/service.ts
+++ src/app/api/service.ts
@@ -3,7 +3,7 @@ const baseUrlImage = "https://image.tmdb.org/t/p/original/"
 
 // Search Movies categories
 const loadApiGenrs = async (gen: string) => {
-    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.KEY_THEMOVIEDB}&with_genres=${gen}&language=pt-BR&page=1`)
+    const response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${process.env.NEXT_KEY_MOVIEDB}&with_genres=${gen}&language=pt-BR&page=1`)
     if(!response.ok) {
       return {message: "Error ao buscar os dados"}
     }
@@ -13,7 +13,7 @@ const loadApiGenrs = async (gen: string) => {
 
 // Search Series categories
 const loadApiSeries = async(gen:string) => {
-  const response = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.KEY_THEMOVIEDB}&with_genres=${gen}&language=pt-BR&page=1`)
+  const response = await fetch(`https://api.themoviedb.org/3/discover/tv?api_key=${process.env.NEXT_KEY_MOVIEDB}&with_genres=${gen}&language=pt-BR&page=1`)
   if(!response.ok) {
     return {message: "Error ao buscar os dados"}
   }
@@ -23,7 +23,7 @@ const loadApiSeries = async(gen:string) => {
 
 // Details series
 const loadDetailsSeries = async (detail: any) => {
-  const response = await fetch(`https://api.themoviedb.org/3/tv/${detail}?api_key=${process.env.KEY_THEMOVIEDB}&language=pt-BR`)
+  const response = await fetch(`https://api.themoviedb.org/3/tv/${detail}?api_key=${process.env.NEXT_KEY_MOVIEDB}&language=pt-BR`)
   if(!response.ok) {
     return {message: "Error ao buscar os dados"}
   }
@@ -32,7 +32,7 @@ const loadDetailsSeries = async (detail: any) => {
 }
 
 const loadDetailsMovies = async (detail: any) => {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/${detail}?api_key=${process.env.KEY_THEMOVIEDB}&language=pt-BR`)
+  const response = await fetch(`https://api.themoviedb.org/3/movie/${detail}?api_key=${process.env.NEXT_KEY_MOVIEDB}&language=pt-BR`)
   if(!response.ok) {
     return {message: "Error ao buscar os dados"}
   }
@@ -41,7 +41,7 @@ const loadDetailsMovies = async (detail: any) => {
 }
 
 const loadNowPlaying = async () => {
-  const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.KEY_THEMOVIEDB}&language=pt-BR`)
+  const response = await fetch(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.NEXT_KEY_MOVIEDB}&language=pt-BR`)
   if(!response.ok) {
     return {message: "Falha ao buscar dados"}
   }
