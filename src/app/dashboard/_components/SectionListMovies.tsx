2677747a97b190b3205bf248fb158102b56a66a8
joaoleandro <new.joaoleandro@gmail.com>
2024-06-08 21:51:41 -0300

    chore: scrolls global and env example

--- src/app/dashboard/_components/SectionListMovies.tsx
+++ src/app/dashboard/_components/SectionListMovies.tsx
@@ -14,7 +14,7 @@ const SectionListMovies: React.FC<SectionListMoviesProps> = ({
     <section className="w-full mt-2">
       <h2 className="text-zinc-400 text-xl font-semibold">{nameCategorie}</h2>
       <div 
-        className="custom-scrollbar flex gap-x-4 overflow-x-hidden md:overflow-x-scroll pt-4 pb-7 relative">
+        className="custom-scrollbar flex gap-x-4 overflow-x-scroll pt-4 pb-7 relative">
         {children}
       </div>
     </section>
