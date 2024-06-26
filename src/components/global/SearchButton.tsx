e084ba18088de5fd4c195e579feeb5adfbd360ba
joaoleandro <new.joaoleandro@gmail.com>
2024-06-26 20:42:07 -0300

    feat: keydown event in input search

--- src/components/global/SearchButton.tsx
+++ src/components/global/SearchButton.tsx
@@ -15,9 +15,18 @@ const SearchButton = () => {
         router.push(`/dashboard/search/${inputSearch}`)
     }
 
+    const keyDownEnter = (e: any) => {
+        if(e.key === "Enter") {
+            if(inputSearch === "") return
+            SetInputSearch("")
+            router.push(`/dashboard/search/${inputSearch}`)
+            return
+        }
+    }
+
     return ( 
         <div className="flex relative items-center justify-center">
-            <Input type="text" placeholder="buscar..." value={inputSearch} onChange={(e: any) => SetInputSearch(e.target.value)} className="h-8 pl-2 pr-6 w-24 md:w-[150px] bg-zinc-600/80 border border-zinc-300"/>
+            <Input type="text" placeholder="buscar..." onKeyDown={keyDownEnter} value={inputSearch} onChange={(e: any) => SetInputSearch(e.target.value)} className="h-8 pl-2 pr-6 w-24 md:w-[150px] bg-zinc-600/80 border border-zinc-300"/>
             
             <button className="absolute right-1" onClick={buttonClick}>
                 <Search size={20} className="w-5 h-5 cursor-pointer text-white hover:text-purple-700 transition duration-200" />
